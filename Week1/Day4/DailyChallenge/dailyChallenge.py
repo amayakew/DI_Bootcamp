d4473599386e0522f69210017af87141a67c6f90
amaya <amaya.kew@gmail.com>
2025-02-06 10:18:00 +0200

    fix dailyChallenge week1 day4

--- Week1/Day4/DailyChallenge/dailyChallenge.py
+++ Week1/Day4/DailyChallenge/dailyChallenge.py
@@ -1,14 +1,32 @@
+# matrix = [
+#     ['7','i','i'],
+#     ['T','s','x'],
+#     ['h','%','?'],
+#     ['i','','#'],
+#     ['s','M',''],
+#     ['$','a',''],
+#     ['#','t','%'],
+#     ['^','r','!']
+#     ]
 
-matrix = [
-    ['7','i','i'],
-    ['T','s','x'],
-    ['h','%','?'],
-    ['i','','#'],
-    ['s','M',''],
-    ['$','a',''],
-    ['#','t','%'],
-    ['^','r','!']
-    ]
+
+matrix_string = '''7ii
+Tsx
+h%?
+i #
+sM 
+$a 
+#t%
+^r!'''
+matrix = []
+
+
+rows = matrix_string.split('\n')
+print(rows)
+
+for row in rows:
+    matrix.append(list(row))
+print(matrix)
 
 alpha = ''
 for col in range(0,3):
