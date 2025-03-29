5feca243b695695efac7073dd265a580a25ec8b8
amaya <amaya.kew@gmail.com>
2025-03-29 23:41:24 +0300

    do dailyChallenge week8 day5

---
+++ Week8/Day5/DailyChallenge/daily-challenge/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [languages, setLanguages] = useState([
+    {name: "Php", votes: 0},
+    {name: "Python", votes: 0},
+    {name: "JavaSript", votes: 0},
+    {name: "Java", votes: 0}
+  ]);
+
+  const updateVotes = (index) => {
+    languages[index].votes += 1;
+    setLanguages([...languages]);
+  };
+
+  return(
+    <>
+      <h1>Vote your language!</h1>
+      <div id="langContainer">
+        {languages.map((lang, index) => (
+          <div key={lang.name} className='lang'><span>{lang.votes}</span><span className='langname'>{lang.name}</span><button type='button' onClick={() => updateVotes(index)} className='btn'>Click Here</button></div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default App
